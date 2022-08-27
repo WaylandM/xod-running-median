f2f112193fdc595c76a98cb7fe738c132ef33289
WaylandM <mw283@cam.ac.uk>
2022-08-27 10:21:03 +0100

    modified:   add/patch.cpp 	modified:   clear/patch.cpp 	new file:   example/patch.xodp 	modified:   get-count/patch.cpp 	modified:   get-highest/patch.cpp 	modified:   get-lowest/patch.cpp 	modified:   get-mean/patch.cpp 	modified:   get-median/patch.cpp 	modified:   get-quantile/patch.cpp 	modified:   get-trimmed-mean/patch.cpp 	modified:   get-trimmed-mean/patch.xodp 	modified:   is-full/patch.cpp modified:   sample-buffer/patch.xodp

--- get-trimmed-mean/patch.cpp
+++ get-trimmed-mean/patch.cpp
@@ -1,10 +1,12 @@
+#pragma XOD evaluate_on_pin disable
+#pragma XOD evaluate_on_pin enable input_UPD
 
 node {
     void evaluate(Context ctx) {
         if (isInputDirty<input_UPD>(ctx)) {
             auto samples = getValue<input_Buffer>(ctx);
-            auto quant = getValue<input_Quant>(ctx);
-            emitValue<output_Value>(ctx, samples -> getQuantile(quant));
+            auto N = getValue<input_N>(ctx);
+            emitValue<output_tMean>(ctx, samples -> getAverage(N));
             emitValue<output_Done>(ctx, 1);
         }
     }
